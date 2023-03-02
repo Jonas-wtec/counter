eeb76b8f5cb5c92dac7cfc0b0350c55312e73a3d
Jonas_H <jhemkendreis@wtec.io>
2023-03-02 23:22:59 +0100

    Testing MatAutoComplete

--- counter-frontend/src/app/app.module.ts
+++ counter-frontend/src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomepageComponent } from './components/homepage/homepage.component';
 import { MatDialogModule } from '@angular/material/dialog';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { ChangeLocationComponent } from './components/change-location/change-location.component';
 import { MatButtonModule } from '@angular/material/button';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
@@ -28,7 +29,9 @@ import { HttpClientModule } from '@angular/common/http';
     MatFormFieldModule,
     MatInputModule,
     MatIconModule,
-    HttpClientModule
+    HttpClientModule,
+    MatAutocompleteModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- counter-frontend/src/app/components/change-location/change-location.component.ts
+++ counter-frontend/src/app/components/change-location/change-location.component.ts
@@ -1,20 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
 import { MatDialogRef } from '@angular/material/dialog';
+import { map, Observable, startWith } from 'rxjs';
+
 
 @Component({
   selector: 'app-change-location',
   templateUrl: './change-location.component.html',
   styleUrls: ['./change-location.component.scss']
 })
-export class ChangeLocationComponent {
+export class ChangeLocationComponent implements OnInit{
 
   name: string | undefined;
+  myControl = new FormControl('');
+  options: string[] = ['One', 'Two', 'Three'];
+  filteredOptions: Observable<string[]> | undefined;
 
   constructor(
     public dialogRef: MatDialogRef<ChangeLocationComponent>,
   ) { }
 
+  ngOnInit(): void {
+    this.filteredOptions = this.myControl.valueChanges.pipe(
+      startWith(''),
+      map(value => this._filter(value || '')),
+    );
+  }
+
   onNoClick(): void {
     this.dialogRef.close();
+  }
+
+  private _filter(value: string): string[] {
+    const filterValue = value.toLowerCase();
+
+    return this.options.filter(option => option.toLowerCase().includes(filterValue));
   }
 }
