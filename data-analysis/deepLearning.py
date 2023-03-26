92876c4786989070f03ae2996d4063e4285d8525
Jonas-wtec <jhemkendreis@wtec.io>
2023-03-26 01:56:15 +0100

    Added first machineLearning algorithm

--- data-analysis/deepLearning.py
+++ data-analysis/deepLearning.py
@@ -1,37 +1,62 @@
 import pandas as pd
 import numpy as np
-import tensorflow as tf
 from sklearn.model_selection import train_test_split
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+import matplotlib.pyplot as plt
+from sklearn.metrics import confusion_matrix
 
-# Load the CSV file into a Pandas DataFrame
-data = pd.read_csv('data.csv', delimiter=';', parse_dates=['time'])
-
-# Drop the 'time' column from the DataFrame
-data = data.drop(['time'], axis=1)
-
-# Preprocess the data
-data = pd.get_dummies(data, columns=['serialNum', 'location'])
-
-# Convert the data to the appropriate data types
-X = data.drop(['count', '_id'], axis=1).astype(np.float32).values
-y = data['count'].astype(np.float32).values
+# Load the data
+df = pd.read_csv('dataContinous.csv', delimiter=';')
 
 # Split the data into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+X = df[['Unix Timestamp', 'motion']]
+y = df['count']
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
-# Define the deep learning model
-model = tf.keras.models.Sequential([
-    tf.keras.layers.Dense(64, activation='relu', input_shape=(X_train.shape[1],)),
-    tf.keras.layers.Dropout(0.2),
-    tf.keras.layers.Dense(1)
+# Normalize the data
+X_mean = X_train.mean()
+X_std = X_train.std()
+X_train = (X_train - X_mean) / X_std
+X_test = (X_test - X_mean) / X_std
+
+# Define the model
+num_classes = len(np.unique(y_train))
+deepLearningModel = Sequential([
+    Dense(32, activation='relu', input_shape=(2,)),
+    Dense(16, activation='relu'),
+    Dense(num_classes, activation='softmax')
 ])
 
 # Compile the model
-model.compile(optimizer='adam', loss='mean_squared_error')
+deepLearningModel.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
 
 # Train the model
-model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=10, batch_size=32)
+history = deepLearningModel.fit(X_train, y_train, epochs=100, batch_size=32, validation_data=(X_test, y_test))
 
-# Evaluate the model on the test set
-loss = model.evaluate(X_test, y_test)
-print('Test set loss:', loss)
+# Evaluate the model
+test_loss, test_acc = deepLearningModel.evaluate(X_test, y_test)
+print('Test accuracy:', test_acc)
+
+# Generate predictions on the test set
+y_pred = np.argmax(deepLearningModel.predict(X_test), axis=-1)
+
+# Create a confusion matrix
+cm = confusion_matrix(y_test, y_pred)
+
+# Plot the confusion matrix
+fig, ax = plt.subplots(figsize=(8, 8))
+ax.imshow(cm, cmap='Oranges')
+ax.grid(False)
+ax.set_xlabel('Estimate Anzahl Personen', fontsize=12, color='black')
+ax.set_ylabel('Echte Anzahl Personen', fontsize=12, color='black')
+ax.xaxis.set(ticks=range(num_classes))
+ax.yaxis.set(ticks=range(num_classes))
+ax.set_xticklabels(['0', '1', '2'])
+ax.set_yticklabels(['0', '1', '2'])
+for i in range(num_classes):
+    for j in range(num_classes):
+        ax.text(j, i, cm[i, j], ha='center', va='center', color='black')
+plt.savefig("ConfusionMatrix.svg")
